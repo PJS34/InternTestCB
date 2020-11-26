7090ef8a1576564aa709040691957ae53f2b89cd
PJS34 <DosSantosJulien77@gmail.com>
2020-11-26 19:25:46 +0100

    Products now display on the screen with the correct date

--- app/src/main/java/com/example/testcb/MainActivity.kt
+++ app/src/main/java/com/example/testcb/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.testcb
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.time.LocalDate
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("NewApi")
@@ -11,9 +15,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FillTableLayout()
+    }
+    fun FillTableLayout(){
+        var table : TableLayout = findViewById(R.id.idTable)
+        var row : TableRow //Ligne
+        var t1 : TextView//Cellules
+        var t2 : TextView
+        for((key, value) in myDatas.ProductMap){
+            println("$key = $value")
+            row = TableRow(this)
 
-        val dateText = myDatas.findShortestDateByGTIN(111111).toString()
-        System.err.println(dateText)
+            //Left cell
+            t1 = TextView(this)
+            t1.setText(key.toString())
+            t1.setGravity(Gravity.CENTER)
+            t1.setLayoutParams(TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1F))
+
+            //right cell
+            t2 = TextView(this)
+            t2.setText(myDatas.findShortestDateByGTIN(key).toString())
+            t2.setGravity(Gravity.CENTER)
+            t2.setLayoutParams(TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1F))
+
+            //add cells in a row
+            row.addView(t1)
+            row.addView(t2)
+
+            //Add row to the table
+            table.addView(row)
+            println("fin")
+        }
+
 
     }
 
--- app/src/main/java/com/example/testcb/ProductContainers.kt
+++ app/src/main/java/com/example/testcb/ProductContainers.kt
@@ -9,36 +9,37 @@ import kotlin.collections.HashMap
 
 @SuppressLint("NewApi")
 class ProductContainers {
-    var ProductMap : HashMap<Int,Array<LocalDate>>
+    var ProductMap : HashMap<Int,ArrayList<LocalDate>>
 
     init {
-        ProductMap = HashMap<Int,Array<LocalDate>>()
-        val ExpiryDate1 = arrayOf(
+        ProductMap = HashMap<Int,ArrayList<LocalDate>>()
+        val ExpiryDate1 = arrayListOf(
             LocalDate.of(2018, 11, 5),
             LocalDate.of(2018, 11, 3),
             LocalDate.of(2018, 11, 2)
 
         )
-        val ExpiryDate2 = arrayOf(
+        val ExpiryDate2 = arrayListOf(
             LocalDate.of(2018, 6, 10),
             LocalDate.of(2018, 6, 6)
         )
-        val ExpiryDate3 = arrayOf(
+        val ExpiryDate3 = arrayListOf(
             LocalDate.of(2018, 12, 31),
             LocalDate.of(2018, 12, 4),
             LocalDate.of(2018, 12, 1)
 
         )
         ProductMap.put(111111,ExpiryDate1)
-        //ProductMap.put(222222,ExpiryDate2)
-        //ProductMap.put(333333,ExpiryDate3)
+        ProductMap.put(222222,ExpiryDate2)
+        ProductMap.put(333333,ExpiryDate3)
     }
 
-    fun addProduct(GTIN : Int,ExpiryDates : Array<LocalDate>){
+    fun addProduct(GTIN : Int,ExpiryDates : ArrayList<LocalDate>){
         ProductMap.put(GTIN,ExpiryDates)
     }
     fun findShortestDateByGTIN(GTIN:Int) : LocalDate? {
        return ProductMap.get(GTIN)?.min()
 
     }
+
 }
