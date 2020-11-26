4527d0fb25ea8954365e2da1c829162cc1627591
PJS34 <DosSantosJulien77@gmail.com>
2020-11-26 17:12:52 +0100

    Structures defined and some func added

--- app/src/main/java/com/example/testcb/MainActivity.kt
+++ app/src/main/java/com/example/testcb/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.testcb
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("NewApi")
+    val myDatas = ProductContainers()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dateText = myDatas.findShortestDateByGTIN(111111).toString()
+        System.err.println(dateText)
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/testcb/ProductContainers.kt
@@ -0,0 +1,44 @@
+package com.example.testcb
+
+import android.annotation.SuppressLint
+import java.time.LocalDate
+import java.time.LocalDate.parse
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
+@SuppressLint("NewApi")
+class ProductContainers {
+    var ProductMap : HashMap<Int,Array<LocalDate>>
+
+    init {
+        ProductMap = HashMap<Int,Array<LocalDate>>()
+        val ExpiryDate1 = arrayOf(
+            LocalDate.of(2018, 11, 5),
+            LocalDate.of(2018, 11, 3),
+            LocalDate.of(2018, 11, 2)
+
+        )
+        val ExpiryDate2 = arrayOf(
+            LocalDate.of(2018, 6, 10),
+            LocalDate.of(2018, 6, 6)
+        )
+        val ExpiryDate3 = arrayOf(
+            LocalDate.of(2018, 12, 31),
+            LocalDate.of(2018, 12, 4),
+            LocalDate.of(2018, 12, 1)
+
+        )
+        ProductMap.put(111111,ExpiryDate1)
+        //ProductMap.put(222222,ExpiryDate2)
+        //ProductMap.put(333333,ExpiryDate3)
+    }
+
+    fun addProduct(GTIN : Int,ExpiryDates : Array<LocalDate>){
+        ProductMap.put(GTIN,ExpiryDates)
+    }
+    fun findShortestDateByGTIN(GTIN:Int) : LocalDate? {
+       return ProductMap.get(GTIN)?.min()
+
+    }
+}
